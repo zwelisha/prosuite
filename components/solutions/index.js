c54890e371c2929556b710a34ad470cb0842a8c4
zwelisha <zwescientist@gmail.com>
2024-03-13 15:55:05 +0200

    Added grey vertical lines separating content, the left borders on solutions section. Hid it on mobile devices

--- components/solutions/index.js
+++ components/solutions/index.js
@@ -2,6 +2,7 @@ import PrimaryButton from "../button";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import VerticalLine from "../vertical-line";
 const Solutions = () => {
     return (
         <div id="solutions-section">
@@ -13,7 +14,7 @@ const Solutions = () => {
                     </Typography>
                 </div>
                 <div>
-                    <Grid container spacing={2} className="mb-50 mt-50">
+                    <Grid container spacing={0} className="mb-50 mt-50">
                         <Grid md={4} item>
                             <h3 className="text-linear-gradient-blue">Asset Management</h3>
                             <p className="text-light-grey text-medium line-height-24">
@@ -21,14 +22,20 @@ const Solutions = () => {
                             </p>
                             <PrimaryButton text="LEARN MORE"/>
                         </Grid>
-                        <Grid md={4} item>
+                        <Grid className="hide-on-mobile" md={1} item sx={{display: 'flex', alignItems: 'center', }}>
+                            <VerticalLine/>
+                        </Grid>
+                        <Grid md={3} item>
                             <h3 className="text-linear-gradient-blue">Risk Management</h3>
                             <p className="text-light-grey text-medium line-height-24 text-left-bordered-grey">
                             Identify, capture and monitor enterprise risks with ease.
                             </p>
                             <PrimaryButton text="LEARN MORE"/>
                         </Grid>
-                        <Grid md={4} item>
+                        <Grid className="hide-on-mobile" md={1} item sx={{display: 'flex', alignItems: 'center', }}>
+                            <VerticalLine/>
+                        </Grid>
+                        <Grid md={3} item>
                             <h3 className="text-linear-gradient-blue">Asset Management</h3>
                             <p className="text-light-grey text-medium line-height-24">
                                 Easy-to-use solution that enables your firm to convert paper into electronic files.
@@ -38,7 +45,7 @@ const Solutions = () => {
                     </Grid>
                 </div>
                 <div>
-                    <Grid container spacing={2} className="mb-50 mt-50">
+                    <Grid container spacing={0} className="mb-50 mt-50">
                         <Grid md={4} item>
                             <h3 className="text-linear-gradient-blue">Perfomance Management</h3>
                             <p className="text-light-grey text-medium line-height-24">
@@ -46,14 +53,20 @@ const Solutions = () => {
                             </p>
                             <PrimaryButton text="LEARN MORE"/>
                         </Grid>
-                        <Grid md={4} item>
+                        <Grid className="hide-on-mobile" md={1} item sx={{display: 'flex', alignItems: 'center', }}>
+                            <VerticalLine/>
+                        </Grid>
+                        <Grid md={3} item>
                             <h3 className="text-linear-gradient-blue">Audit</h3>
                             <p className="text-light-grey text-medium line-height-24">
                                 Streamline your audit processes and comply with regulations or internal policies.
                             </p>
                             <PrimaryButton text="LEARN MORE"/>
                         </Grid>
-                        <Grid md={4} item>
+                        <Grid className="hide-on-mobile" md={1} item sx={{display: 'flex', alignItems: 'center', }}>
+                            <VerticalLine/>
+                        </Grid>
+                        <Grid md={3} item>
                             <h3 className="text-linear-gradient-blue">Compliance Management</h3>
                             <p className="text-light-grey text-medium line-height-24">
                                 Manage and meet legal requirements and comply with applicable laws and regulations.
---
+++ components/vertical-line/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VerticalLine = ({ height }) => {
+    return (
+        <table style={{ height: height || '85px', marginLeft: '35px' }}>
+        <tbody>
+            <tr>
+            <td style={{ width: '2px', backgroundColor: '#E5E5E5',}}></td>
+            </tr>
+        </tbody>
+        </table>
+    );
+};
+
+export default VerticalLine;
